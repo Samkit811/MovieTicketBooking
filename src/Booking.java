375d9c3e548eccbec35566eb3c7f5d2f5f8dea45
samkits <samkitsalot0@gmail.com>
2025-04-24 19:16:10 +0530

    Added Seat class and it's logic in Booking class

--- src/Booking.java
+++ src/Booking.java
@@ -1,2 +1,13 @@
+import java.util.Random;
+
 public class Booking {
+    private Integer id;
+    private Seat seat;
+    private Random random;
+
+    public Booking(Seat seat){
+        this.seat = seat;
+        this.random = new Random();
+        this.id = this.random.nextInt(100);
+    }
 }
---
+++ src/Seat.java
@@ -0,0 +1,11 @@
+public class Seat {
+    private Person person;
+    private Integer id;
+
+    public Seat(Person person, Integer id){
+        this.id = id;
+        this.person = person;
+    }
+
+
+}
