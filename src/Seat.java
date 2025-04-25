2a0df25424f4971271f1875ef6aff2009cbcf97e
samkits <samkitsalot0@gmail.com>
2025-04-25 10:27:42 +0530

    Updated seat class

--- src/Seat.java
+++ src/Seat.java
@@ -1,10 +1,12 @@
 public class Seat {
     private Person person;
     private Integer id;
+    private Show show;
 
-    public Seat(Person person, Integer id){
+    public Seat(Person person, Integer id, Show show){
         this.id = id;
         this.person = person;
+        this.show = show;
     }
 
 
